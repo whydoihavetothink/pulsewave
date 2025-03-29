30828aa70c5f2c4fe7b4f09eef6e6d8b749b2592
Bui Hoang Trung <xbui@fi.muni.cz>
2025-03-29 23:43:43 +0100

    hero section cleanup

--- src/app/[locale]/hero.tsx
+++ src/app/[locale]/hero.tsx
@@ -33,23 +33,13 @@ function Hero() {
         poster=""
         className="h-full w-full object-center object-cover absolute block lg:hidden"
       >
-        <source src="media/ARGY-PROMO-V3-mobile.webm" type="video/webm" />
         <source src="media/ARGY-PROMO-V3-mobile.mp4" type="video/mp4" />
+        <source src="media/ARGY-PROMO-V3-mobile.webm" type="video/webm" />
         Your browser does not support the video tag.
       </video>
       <div className="absolute inset-0 h-full w-full bg-gray-900/60" />
       <div className="grid min-h-screen px-8">
         <div className="container relative z-10 my-auto mx-auto grid place-items-center text-center">
-          <Typography
-            variant="h3"
-            color="white"
-            className="mb-2"
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            ARGY @ SAT / JULY 19.2025
-          </Typography>
           <div className="relative w-full h-48 overflow-hidden flex justify-center items-center">
             <Image
               width={1200}
@@ -59,16 +49,6 @@ function Hero() {
               className="absolute w-400 h-full object-cover"
             />
           </div>
-          <Typography
-            variant="lead"
-            color="white"
-            className="mt-1 mb-12 w-full md:max-w-full lg:max-w-2xl"
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            KRIZIKOVA FONTANA PRAGUE
-          </Typography>
             <a href={TICKETS_LINK} target="_blank">
               <Button
                 className={`font-bold text-lg`}
